6654292a152b66894d5cbb85058daeee2d98bed0
sharnoff <git@sharnoff.io>
2021-03-03 18:27:40 -0800

    add conditional struct parsing with `#[parse_if]`

--- src/tests.rs
+++ src/tests.rs
@@ -36,8 +36,8 @@ test_all! {
         }",
         "impl ::syn::parse::Parse for Foo {
             fn parse(__parse_input: ::syn::parse::ParseStream) -> ::syn::Result<Self> {
-                let bar = __parse_input.parse()?;
-                let baz = __parse_input.parse()?;
+                let bar: Bar = __parse_input.parse()?;
+                let baz: Baz = __parse_input.parse()?;
 
                 Ok(Foo {
                     bar,
@@ -58,9 +58,9 @@ test_all! {
         where <B as Bar>::Qux: Quack,
         {
             fn parse(__parse_input: ::syn::parse::ParseStream) -> ::syn::Result<Self> {
-                let bar = __parse_input.parse()?;
-                let baz = __parse_input.parse()?;
-                let quacker = __parse_input.parse()?;
+                let bar: B = __parse_input.parse()?;
+                let baz: Baz = __parse_input.parse()?;
+                let quacker: Q = __parse_input.parse()?;
 
                 Ok(Foo {
                     bar,
@@ -80,11 +80,13 @@ test_all! {
         }",
         "impl ::syn::parse::Parse for Foo {
             fn parse(__parse_input: ::syn::parse::ParseStream) -> ::syn::Result<Self> {
-                let bar = __parse_input.parse()?;
+                let bar: Bar = __parse_input.parse()?;
 
                 let __paren_backing_token_stream;
-                let paren = ::syn::parenthesized!(__paren_backing_token_stream in __parse_input);
-                let baz = __paren_backing_token_stream.parse()?;
+                let paren: syn::token::Paren =
+                    ::syn::parenthesized!(__paren_backing_token_stream in __parse_input);
+                let baz: Baz =
+                    __paren_backing_token_stream.parse()?;
 
                 Ok(Foo {
                     bar,
@@ -107,15 +109,17 @@ test_all! {
         }",
         "impl ::syn::parse::Parse for Foo {
             fn parse(__parse_input: ::syn::parse::ParseStream) -> ::syn::Result<Self> {
-                let bar = __parse_input.parse()?;
+                let bar: Bar = __parse_input.parse()?;
 
                 let __fst_backing_token_stream;
-                let fst = ::syn::bracketed!(__fst_backing_token_stream in __parse_input);
+                let fst: syn::token::Bracket =
+                    ::syn::bracketed!(__fst_backing_token_stream in __parse_input);
 
                 let __snd_backing_token_stream;
-                let snd = ::syn::braced!(__snd_backing_token_stream in __fst_backing_token_stream);
+                let snd: syn::token::Brace =
+                    ::syn::braced!(__snd_backing_token_stream in __fst_backing_token_stream);
 
-                let baz = __snd_backing_token_stream.parse()?;
+                let baz: Baz = __snd_backing_token_stream.parse()?;
 
                 Ok(Foo {
                     bar,
@@ -134,9 +138,9 @@ test_all! {
         }",
         "impl ::syn::parse::Parse for Foo {
             fn parse(__parse_input: ::syn::parse::ParseStream) -> ::syn::Result<Self> {
-                let bar = __parse_input.parse()?;
+                let bar: Bar = __parse_input.parse()?;
                 
-                let baz = match (|p| !p.is_empty())(__parse_input) {
+                let baz: Baz = match (|p| !p.is_empty())(__parse_input) {
                     true => Some(__parse_input.parse()?),
                     false => None,
                 };
@@ -144,6 +148,28 @@ test_all! {
                 Ok(Foo {
                     bar,
                     baz,
+                })
+            }
+        }"
+    },
+    parse_if_peek: {
+        "struct Foo {
+            at_symbol: Option<Token![@]>,
+            #[parse_if(at_symbol.is_some())]
+            name: Ident,
+        }",
+        "impl ::syn::parse::Parse for Foo {
+            fn parse(__parse_input: ::syn::parse::ParseStream) -> ::syn::Result<Self> {
+                let at_symbol: Option<Token![@]> = __parse_input.parse()?;
+
+                let name: Ident = match at_symbol.is_some() {
+                    true => Some(__parse_input.parse()?),
+                    false => None,
+                };
+                
+                Ok(Foo {
+                    at_symbol,
+                    name,
                 })
             }
         }"
